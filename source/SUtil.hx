c377a97fcbbf1e4c13eebb6c696553a85caf7328
Hiho2950 <109638489+Hiho2950@users.noreply.github.com>
2022-07-31 02:59:09 +0100

    Update SUtil.hx

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -1,40 +1,28 @@
 package;
 
 #if android
-import android.Tools;
 import android.Permissions;
 import android.PermissionsList;
+import android.os.Build.VERSION;
+import android.os.Environment;
 #end
+import flash.system.System;
+import flixel.FlxG;
+import flixel.util.FlxStringUtil;
+import haxe.CallStack.StackItem;
+import haxe.CallStack;
+import haxe.io.Path;
 import lime.app.Application;
 import openfl.events.UncaughtErrorEvent;
 import openfl.utils.Assets as OpenFlAssets;
 import openfl.Lib;
-import haxe.CallStack.StackItem;
-import haxe.CallStack;
-import haxe.io.Path;
 import sys.FileSystem;
 import sys.io.File;
-import flash.system.System;
 
 using StringTools;
 
 class SUtil
 {
-	#if android
-	private static var aDir:String = null; // android dir
-	#end
-
-	public static function getPath():String
-	{
-		#if android
-		if (aDir != null && aDir.length > 0)
-			return aDir;
-		else
-			return aDir = Tools.getExternalStorageDirectory() + '/' + '.' + Application.current.meta.get('file') + '/';
-		#else
-		return '';
-		#end
-	}
 
 	public static function doTheCheck()
 	{
@@ -75,6 +63,14 @@ class SUtil
 		}
 		#end
 	}
+	
+	public static function getPath():String
+	{
+		#if android
+		return Environment.getExternalStorageDirectory() + '/' + '.' + Application.current.meta.get('file') + '/';
+		#else
+		return '';
+		#end
 
 	public static function gameCrashCheck()
 	{
