8a70797e5baa8d555f04b643efc58f6eb2c00e59
Hiho2950 <109638489+Hiho2950@users.noreply.github.com>
2022-07-31 03:00:37 +0100

    Update SUtil.hx

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -6,35 +6,22 @@ import android.PermissionsList;
 import android.os.Build.VERSION;
 import android.os.Environment;
 #end
+import flash.system.System;
+import flixel.FlxG;
+import flixel.util.FlxStringUtil;
+import haxe.CallStack.StackItem;
+import haxe.CallStack;
+import haxe.io.Path;
 import lime.app.Application;
 import openfl.events.UncaughtErrorEvent;
 import openfl.utils.Assets as OpenFlAssets;
 import openfl.Lib;
-import haxe.CallStack.StackItem;
-import haxe.CallStack;
-import haxe.io.Path;
 import sys.FileSystem;
 import sys.io.File;
-import flash.system.System;
-
-using StringTools;
 
 class SUtil
 {
-	#if android
-	private static var aDir:String = null; // android dir
-	#end
-
-	public static function getPath():String
-	{
-		#if android
-		if (aDir != null && aDir.length > 0)
-			return aDir;
-		#else
-		return '';
-		#end
-	}
-
+	
 	public static function doTheCheck()
 	{
 		#if android
@@ -86,6 +73,15 @@ class SUtil
 				}
 			}
 		}
+		#end
+	}
+	
+	public static function getPath():String
+	{
+		#if android
+		return Environment.getExternalStorageDirectory() + '/' + '.' + Application.current.meta.get('file') + '/';
+		#else
+		return '';
 		#end
 	}
 
